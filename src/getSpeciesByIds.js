d36b14b68084ce19b624c6775f81f0b944476fe5
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-03-16 16:49:22 -0300

    Desafio 1, obrigatorio - concluido, feito e testado.

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  if (ids === []) {
+    return undefined;
+  }
+  const description = [];
+  ids.forEach((param) => {
+    data.species.forEach((animal) => {
+      if (animal.id === param) {
+        description.push(animal);
+      }
+    });
+  });
+  return description;
 }
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
