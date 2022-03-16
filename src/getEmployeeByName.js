c5515e84bf090e8abe7966c58154dd4f4da677dc
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-03-16 18:37:28 -0300

    Desafio 3, obrigatorio - concluido, feito e testado.

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,13 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  let result = {};
+  data.employees.forEach((employee) => {
+    if (employee.firstName === employeeName || employee.lastName === employeeName) {
+      result = employee;
+    }
+  });
+  return result;
 }
 
 module.exports = getEmployeeByName;
