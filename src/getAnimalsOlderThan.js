819aa6ea134b9ae0d026cbc2d48e634d3dcaaead
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-03-16 18:08:54 -0300

    Desafio 2, obrigatorio - concluido, feito e testado.

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  let result;
+  data.species.forEach((animalName) => {
+    if (animalName.name === animal) {
+      result = animalName.residents
+        .map((criatura) => criatura.age)
+        .every((idade) => idade >= age);
+    }
+  });
+  return result;
 }
 
 module.exports = getAnimalsOlderThan;
