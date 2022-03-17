8525d721bd64bd50d68365f5818c740a03801fda
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-03-17 14:30:34 -0300

    Desafio 4, obrigatorio - concluido, feito e testado.

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,30 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  let result = false;
+  const managersArray = data.employees.map((employee) => employee.managers);
+  managersArray.forEach((manager) => {
+    if (manager.indexOf(id) !== -1) {
+      result = true;
+    }
+  });
+  return result;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const result = [];
+  if (isManager(managerId)) {
+    data.employees.forEach((employee) => {
+      const contains = employee.managers.indexOf(managerId);
+      if (contains !== -1) {
+        const managed = [employee.firstName, employee.lastName];
+        result.push(managed.join(' '));
+      }
+    });
+    return result;
+  }
+
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
