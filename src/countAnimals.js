d24415b43403bea4a24a666b261fb80c91685f66
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-03-17 16:07:21 -0300

    Desafio 5, obrigatorio - concluido, feito e testado.

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,44 @@
 const data = require('../data/zoo_data');
 
+const justKind = (nameAnimal) => {
+  let answer = 0;
+  data.species.forEach((especie) => {
+    if (especie.name === nameAnimal) {
+      answer = especie.residents.length;
+    }
+  });
+  return answer;
+};
+
+const animalAndSex = (nameAnimal, sexAnimal) => {
+  let count = 0;
+  data.species.forEach((especie) => {
+    if (especie.name === nameAnimal) {
+      especie.residents.forEach((infoAnimal) => {
+        if (infoAnimal.sex === sexAnimal) {
+          count += 1;
+        }
+      });
+    }
+  });
+  return count;
+};
+
 function countAnimals(animal) {
-  // seu código aqui
+  let result = {};
+  if (typeof animal === 'object') {
+    const infoAnimal = Object.values(animal);
+    if (infoAnimal.length === 1) {
+      result = justKind(infoAnimal[0]);
+      return result;
+    }
+    result = animalAndSex(infoAnimal[0], infoAnimal[1]);
+    return result;
+  }
+  data.species.forEach((bicho) => {
+    result[bicho.name] = bicho.residents.length;
+  });
+  return result;
 }
 
 module.exports = countAnimals;
