1343b2c0675d8dfca77c5c378f8517860a4f3011
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-03-18 14:02:48 -0300

    Desafio 9, obrigatorio - concluido, feito e testado.

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,28 @@
 const data = require('../data/zoo_data');
 
+const selectAnimalList = (employeeId) => {
+  const infoEmployee = data.employees.find((employee) => employee.id === employeeId);
+  const speciesID = infoEmployee.responsibleFor[0];
+  const species = data.species.find((speciesId) => speciesId.id === speciesID);
+  return species.residents;
+};
+
+const olderAnimal = (arrayAnimals) => {
+  let olderAge = 0;
+  let selectedAnimal;
+  arrayAnimals.forEach((animal) => {
+    if (animal.age > olderAge) {
+      olderAge = animal.age;
+      selectedAnimal = animal;
+    }
+  });
+  return selectedAnimal;
+};
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  let result = selectAnimalList(id);
+  result = olderAnimal(result);
+  return [result.name, result.sex, result.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
